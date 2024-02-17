63059710a462c7265ab241e09ccd28e953987fb4
Chamali Vishmani <chamali106astro@gmail.com>
2024-02-17 19:14:09 +0530

    inpainting

--- fcc-LangChain_Gen_AI/app.py
+++ fcc-LangChain_Gen_AI/app.py
@@ -47,8 +47,8 @@ def get_root_words(prompt):
     return refined_text
 
 
-st.set_page_config(page_title="Q&A Demo")
-st.header("Langchain application")
+st.set_page_config(page_title="Text prompt to Image AI")
+st.header("Generate AI garment design from text and image")
 
 user_text_input = st.text_input("Enter design text:", key="input")
 
@@ -75,10 +75,11 @@ if refine_text_prompt_btn:
     st.write(refined_text_prompt)
     # st.write(keywords)
 
-if refined_text_prompt is not None:
-    generate_image_btn = st.button("Generate Image")
+# if refined_text_prompt is not None:
+#     generate_image_btn = st.button("Generate Image")
+    with st.spinner("Generating Image..."):
+        # if generate_image_btn:
 
-    if generate_image_btn:
         output = replicate.run(
             "konieshadow/fooocus-api:fda927242b1db6affa1ece4f54c37f19b964666bf23b0d06ae2439067cd344a4",
             input={
@@ -107,18 +108,21 @@ if refined_text_prompt is not None:
         )
         print(output)
         st.image(output[0])
-        st.write(output[0])
+        # st.write(output[0])
         # st.download_button(label="Download Image",
         #                    data=output, file_name="image.jpg")
-        st.markdown(f"[Download Image]({output[0]})", unsafe_allow_html=True)
-        st.download_button(label="Download Image", data=output[0])
+        st.markdown(
+            f"[Download Image]({output[0]})", unsafe_allow_html=True)
+        # st.download_button(label="Download Image", data=output[0])
 
-img_input = st.file_uploader("Upload an image", type=["jpg", "png"])
+img_input = st.file_uploader(
+    "Upload an image to make a variation", type=["jpg", "png"])
 model = genai.GenerativeModel('gemini-pro-vision')
 if img_input:
     img = PIL.Image.open(img_input)
-    img.show()
+    st.image(img)
     about_img = model.generate_content(img)
+    st.subheader("About the image uploaded : ")
     st.write(about_img.text)
 
 variation_input = st.text_input("Variation input:", key="variation_input")
@@ -126,9 +130,46 @@ variation_Btn = st.button("Make variation")
 variation_keywords = get_root_words(variation_input)
 
 if variation_Btn:
-    st.write("Variation")
+    st.subheader("The variation text prompt is:")
+    prompt_for_prompt = f"Generate a text prompt for an text+image to image generation AI model to create a unique garment design by varying image given. Consider including keywords related to the style, theme, and features  in the design. Use the user's input for inspiration: '{variation_input}'."
     response = model.generate_content(
-        ["use keywords "+variation_keywords+" and write a image generation text prompt for the image given with the conetxt"+variation_input, img], stream=True)
+        [prompt_for_prompt, img], stream=True)
     response.resolve()
     st.write(response.text)
-    st.write(response.candidates)
+
+    with st.spinner("Generating Image..."):
+        output = replicate.run(
+            "konieshadow/fooocus-api:fda927242b1db6affa1ece4f54c37f19b964666bf23b0d06ae2439067cd344a4",
+            input={
+                "prompt": variation_input,
+                "cn_type1": "ImagePrompt",
+                "cn_type2": "ImagePrompt",
+                "cn_type3": "ImagePrompt",
+                "cn_type4": "ImagePrompt",
+                "sharpness": 2,
+                "image_seed": 50403806253646856,
+                "uov_method": "Disabled",
+                "image_number": 1,
+                "guidance_scale": 4,
+                "refiner_switch": 0.5,
+                "style_selections": "Fooocus V2,Fooocus Enhance,Fooocus Sharp",
+                "uov_upscale_value": 0,
+                "inpaint_input_image": img_input,
+                "outpaint_selections": "",
+                "outpaint_distance_top": 0,
+                "performance_selection": "Speed",
+                "outpaint_distance_left": 0,
+                "aspect_ratios_selection": "1152*896",
+                "outpaint_distance_right": 0,
+                "outpaint_distance_bottom": 0,
+                "inpaint_additional_prompt": "response.text"
+            }
+        )
+    print(output)
+    st.image(output[0])
+    # st.write(output[0])
+    # st.download_button(label="Download Image",
+    #                    data=output, file_name="image.jpg")
+    st.markdown(
+        f"[Download Image]({output[0]})", unsafe_allow_html=True)
+    # st.download_button(label="Download Image", data=output[0])
